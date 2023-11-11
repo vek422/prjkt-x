fbdc1639ec69547b0c86d483fbf03cde17a5e508
amol <vedantkotkar111@gmail.com>
2023-11-11 19:22:47 +0530

    REMOVE node_modules

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -46,7 +46,7 @@ const Login = async (req, res) => {
     user = user.toObject();
     delete user.password;
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRETE_KEY);
-    res.status(200).json({ token, user }).end();
+    res.status(201).json({ token, user }).end();
   } catch (err) {
     res.status(500).json({ message: err }).end();
   }
